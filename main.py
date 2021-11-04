c5cf334cad21f83d3d02a7f0da20cd49daf4bb34
artem <vrail30@outlook.com>
2021-11-04 19:36:26 +0300

    Task corrected.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os.path
 from contextlib import contextmanager
-from os import getcwd, chdir
+from os import chdir, getcwd
 
 
 @contextmanager
 def cd_context(path: str):
     current_dir = getcwd()
-    try:
-        if not os.path.isdir(path) or not os.path.exists(path):
-            raise ValueError
-        chdir(path)
 
-    finally:
-        chdir(current_dir)
+    if not os.path.isdir(path) or not os.path.exists(path):
+        raise ValueError
+    chdir(path)
+    yield f"Directory changed to {path}"
+    chdir(current_dir)
